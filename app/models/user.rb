0e08db8696079f584b92cd1abc850d0c52fd4d5f
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-10-04 20:10:56 +0100

    Updated model with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_one_attached :photo, dependent: :destroy
+  validate :name, :email_address, :password, presence: true
 end
