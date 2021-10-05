e2a683d0ace3e79ae8af9dfb21679c48203b5ea6
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-10-05 19:25:44 +0100

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[show create update destroy] do
+    resources :posts, only: %i[index show create update destroy]
+  end
 end
